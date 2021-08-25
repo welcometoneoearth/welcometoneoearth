b3c68ab600131d945d617b5d68e7b5adab494221
notramdass <notramdass@gmail.com>
2021-08-25 16:13:29 -0600

    lots

---
+++ script.js
@@ -0,0 +1,10 @@
+let password;
+let pass1="password";
+
+password = prompt('If you want Premium Content, enter the Super Secret Patreon Password! Otherwise, enter anything.','poop');
+
+if (password==pass1) {
+  alert('Thank you Citizen B-138D2');
+} else {
+  window.location="http://welcometoneoearth.com/nonpremium";
+}
